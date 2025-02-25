590f61e898a644d44e0197f068f26d4c7f8c1885
Fadi Atieh <fadi.atieh98@gmail.com>
2025-02-25 21:51:48 +0000

    Remove # before phonemizing.

--- phonemize.py
+++ phonemize.py
@@ -6,6 +6,6 @@ def phonemize(text, global_phonemizer, tokenizer):
     tokens = tokenizer.tokenize(text)
 
     tokens_ids = [tokenizer.convert_tokens_to_ids(token) for token in tokens]
-    phonemes = [global_phonemizer.phonemize([token], strip=True)[0] if token not in string.punctuation else token for token in tokens]
+    phonemes = [global_phonemizer.phonemize([token.replace("#", "")], strip=True)[0] if token not in string.punctuation else token for token in tokens]
         
     return {'tokens_ids' : tokens_ids, 'phonemes': phonemes}
